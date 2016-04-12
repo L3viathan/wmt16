6f2bbe66adcbfaa019fb271dff030893955a6add
L3viathan <git@l3vi.de>
2016-04-12 16:39:55 +0200

    heaps for top N, scorers evaluated seperately

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
 import gzip
 import glob
-import threading
+import heapq
 import itertools
-import subprocess
-from collections import namedtuple
+from operator import itemgetter
+from collections import namedtuple, defaultdict
 from base64 import b64decode
 
 import simple
@@ -37,39 +37,44 @@ def read_gold(filename):
 def log(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+def recall_at_n(n, assignments, gold):
+    return sum(
+            1
+            for key in gold
+            if gold[key] in map(itemgetter(1), heapq.nlargest(n, assignments[key]))
+            ) / len(gold)
+
 
 if __name__ == '__main__':
+    # evaluation data
+    gold = read_gold("data/train.pairs")
+
     # get input data, file by file.
     for filename in glob.iglob("data/*.lott.gz"):
         log("Now working on {}".format(filename))
         pairs = itertools.combinations(get_pages(filename), 2)
 
-    for fltr in filters:
-        # set up filters
-        pairs = fltr(pairs)
+        for fltr in filters:
+            # set up filters
+            pairs = fltr(pairs)
 
-    for scorer in scorers:
-        # set up scorers
-        pairs = scorer(pairs)
+        pairs = list(pairs)  # run all filters
 
-    pairs = list(pairs)
+        for scorer in scorers:
+            # set up scorers
+            top_n_size = 10
+            assignments = defaultdict(list)
+            for pair, score in scorer(pairs):
+                left, right = pair
+                if len(assignments[left]) < top_n_size:
+                    heapq.heappush(assignments[left], (score, right))
+                else:
+                    heapq.heappushpop(assignments[left], (score, right))
 
-    """
-    for pair, score in pairs:
-        left, right = pair
-        print(left.url, right.url, score)
-    """
+            # now we have a dictionary from source document to a heap of (up to)
+            # 10 target documents (with score).
 
-    threshold = 0.5  # later: learned
 
-    # evaluate
-    gold = read_gold("data/train.pairs")
-    ours = {}
-
-    for pair, score in pairs:
-        left, right = pair
-        if score > threshold and left not in ours:
-            ours[left] = right
-
-    recall = sum(1 for key in gold if gold[key] == ours.get(key, None)) / len(gold)
-    print("Recall:", recall)
+            print("Recall@1:", recall_at_n(1, assignments, gold))
+            print("Recall@5:", recall_at_n(5, assignments, gold))
+            print("Recall@10:", recall_at_n(10, assignments, gold))
