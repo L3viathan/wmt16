3fb0b9c6f52be15466a47f1b01c320fede319f30
L3viathan <git@l3vi.de>
2016-05-02 01:27:48 +0200

    just in case I can't use my computer tomorrow

--- combination/top_1_stringmatching_2.py
+++ combination/top_1_stringmatching_2.py
@@ -1,12 +1,24 @@
 import sys
+from math import log
 from Levenshtein import distance
 from collections import Counter
 scores = {}
 
+min_scores = {}
+len_scores = {}
+
 threshold = 0.35
+threshold_m=-3000
 
 def fqdn(url):
     return url.split("/")[2]  # very dirty, but works
+
+with open("train.tran.miss.clues.tsv") as f:
+    next(f)  # skip header
+    for line in f:
+        min_score, length, *_, url = line.strip().split()
+        min_scores[url] = float(min_score)
+        len_scores[url] = float(length)
 
 with open("missing_translations.txt") as f:
     for line in f:
@@ -20,10 +32,15 @@ with open("than_top10_training.txt") as than:
             _, source = line.strip().split()
             next(than)  # ignore gold
             domain = fqdn(source)
-            if scores[domain] < threshold:
+            '''
+            if scores[domain] < threshold or abs(min_scores[source]-
                 continue
+            '''
             score1, match1 = next(than).strip().split()
             score2, match2 = next(than).strip().split()
+            #print((float(score1[:-1]) - min_scores[source]) - len_scores[source], file=sys.stderr)
+            if ((float(score1[:-1]) - min_scores[source]) - len_scores[source]) < threshold_m:
+                continue
             if abs(float(score1[:-1])-float(score2[:-1])) < 70:
                 print(source, min([match1, match2], key=lambda x: distance(x, source)), sep="\t")
             else:
@@ -41,6 +58,6 @@ with open("hoa_top10_training.txt") as hoa:
         if source in seen:
             continue
         domain = fqdn(source)
-        if scores[domain] < threshold:
-            print(source, match, sep="\t")
-            seen.add(source)
+        #if scores[domain] < threshold:
+        print(source, match, sep="\t")
+        seen.add(source)
